39be270453a2b1d427bd594d0d8af3b75ee9c133
prioritize <jared@prototypic.al>
2024-05-31 16:37:23 -0700

    Learning pwm

--- src/bin/servo_controller.rs
+++ src/bin/servo_controller.rs
@@ -3,15 +3,35 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::exti::ExtiInput;
+use embassy_stm32::gpio::OutputType;
+use embassy_stm32::gpio::{AnyPin, Pin, Pull};
+use embassy_stm32::gpio::{Input, Level, Output, Speed};
 use embassy_stm32::rcc::SupplyConfig;
 use embassy_stm32::time::Hertz;
+use embassy_stm32::timer::simple_pwm::PwmPin;
 use embassy_stm32::Config;
-use embassy_time::Timer;
+use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
+#[embassy_executor::task]
+async fn led_1_task(led: AnyPin) {
+    let mut led = Output::new(led, Level::High, Speed::Low);
+    loop {
+        Timer::after(Duration::from_millis(500)).await;
+        led.toggle();
+    }
+}
+#[embassy_executor::task]
+async fn led_2_task(led: AnyPin) {
+    let mut led = Output::new(led, Level::Low, Speed::Low);
+    loop {
+        Timer::after(Duration::from_millis(500)).await;
+        led.toggle();
+    }
+}
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let mut config = Config::default();
     config.rcc.supply_config = SupplyConfig::DirectSMPS;
     {
@@ -37,19 +57,18 @@ async fn main(_spawner: Spawner) {
         config.rcc.voltage_scale = VoltageScale::Scale0;
     }
     let p = embassy_stm32::init(config);
+    let pwm = PwmPin::new_ch3(p.PA2, OutputType::PushPull);
+    spawner.spawn(led_1_task(p.PI12.degrade())).unwrap();
+    spawner.spawn(led_2_task(p.PI13.degrade())).unwrap();
     info!("Hello World!");
 
-    let mut led = Output::new(p.PI12, Level::High, Speed::Low);
+    let mut led_3 = Output::new(p.PI14, Level::High, Speed::Low);
 
+    let button_1 = Input::new(p.PK2, Pull::Up);
+    let mut button_1 = ExtiInput::new(button_1, p.EXTI2);
     loop {
-        info!("high");
-        led.set_high();
-        Timer::after_millis(500).await;
-
-        info!("low");
-        led.set_low();
-        Timer::after_millis(500).await;
+        button_1.wait_for_falling_edge().await;
+        info!("Button fired");
+        led_3.toggle();
     }
 }
-
-
